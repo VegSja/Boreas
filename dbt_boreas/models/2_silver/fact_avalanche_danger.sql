e07386cbd3581dcfc5e49db628ce389aff800a41
Vegard Sjåvik <vgsjaavik@gmail.com>
2026-01-15 12:54:59 +0100

    Filtering in fact tables

--- dbt_boreas/models/2_silver/fact_avalanche_danger.sql
+++ dbt_boreas/models/2_silver/fact_avalanche_danger.sql
@@ -4,5 +4,12 @@
     )
 }}
 
-SELECT * 
+SELECT 
+    reg_id AS registration_id,
+    region_id,
+    danger_level,
+    valid_from,
+    valid_to,
+    publish_time,
+    main_text
 FROM {{ source('1_bronze', 'avalanche_danger_levels') }}
--- dbt_boreas/models/2_silver/fact_weather.sql
+++ dbt_boreas/models/2_silver/fact_weather.sql
@@ -6,18 +6,35 @@
 
 WITH historic AS (
     SELECT *,
-    'historic' AS type
+    'historic' AS weather_type
     FROM {{ source('1_bronze', 'weather_historic') }}
+    WHERE "time" < today()
 ), 
 
 forecast AS (
     SELECT *,
-    'forecast' AS type
+    'forecast' AS weather_type
     FROM {{ source('1_bronze', 'weather_forecast') }}
-) 
+    WHERE "time" > today()
+),
 
-SELECT *
-FROM historic
-UNION ALL 
-SELECT *
-FROM forecast
+unioned AS (
+    SELECT *
+    FROM historic
+    UNION ALL 
+    SELECT *
+    FROM forecast
+)
+
+SELECT 
+    time,
+    temperature_2m,
+    relative_humidity_2m,
+    precipitation,
+    windspeed_10m,
+    loaded_at, --TODO: Needed?
+    region_id,
+    weather_type,
+FROM unioned
+
+
